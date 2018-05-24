b0cb6b4e93ec07b5be5d5300209f6de5aa941147
Fabio Mora <fabiomoi022@gmail.com>
2018-05-23 23:17:37 -0600

    TESTS

--- src/tec/ic/ia/pc2/test_genetic.py
+++ src/tec/ic/ia/pc2/test_genetic.py
@@ -37,28 +37,76 @@ def get_one_carrot_input():
     return result
 
 
-class TestCompletness(object):
+class TestGeneral(object):
     """ Test object for proof that the algorithm actually CAN finish """
 
-    def test_completeness_one(self):
+    def test_comparation(self):
         """
-        Two present carrots and bunny needs two carrots
+        Comparation between indiviuals that gives a 5
         """
         result = get_two_carrot_input()
-        individual_1 = [['>','C',' ','<'],
-                     [' ','Z',' ',' '],
-                     [' ',' ',' ',' '],
-                     [' ',' ','Z',' ']]
-        individual_2 = [[' ','C',' ','<'],
-                     [' ','Z',' ',' '],
-                     [' ',' ',' ',' '],
-                     [' ',' ','Z',' ']]
+        individual_1 = [['>', 'C', ' ', '<'],
+                        [' ', 'Z', ' ', ' '],
+                        [' ', ' ', ' ', ' '],
+                        [' ', ' ', 'Z', ' ']]
+        individual_2 = [[' ', 'C', ' ', '<'],
+                        [' ', 'Z', ' ', ' '],
+                        [' ', ' ', ' ', ' '],
+                        [' ', ' ', 'Z', ' ']]
         assert 5 == result.compare(individual_1, individual_2)
 
-    def test_completeness_two(self):
+    def test_completeness(self):
         """
-        One present carrots but bunny needs two
+        Tests if the algorithm gives a result that can be called a result
         """
         result = get_two_carrot_input()
         res = result.run()
         assert res[-1] > 10000
+
+    def test_population_generation(self):
+        """
+        Tests if the generation of the first population acts correctly
+        """
+        result = get_two_carrot_input()
+        res = result.generate_first_population(12)
+        assert len(res) == 12
+
+    def test_bad_fitness(self):
+        """
+        Tests if the generation of the first population acts correctly
+        """
+        result = get_two_carrot_input()
+        individual_1 = [['>', 'C', ' ', '<'],
+                        [' ', 'Z', ' ', ' '],
+                        [' ', ' ', ' ', ' '],
+                        [' ', ' ', 'Z', ' ']]
+        res = result.fitness(individual_1)
+        assert res < 0
+
+    def test_good_fitness(self):
+        """
+        Tests if the generation of the first population acts correctly
+        """
+        result = get_two_carrot_input()
+        individual_1 = [['>', 'C', ' ', 'v'],
+                        [' ', 'Z', ' ', ' '],
+                        [' ', ' ', ' ', ' '],
+                        [' ', ' ', 'Z', '<']]
+        res = result.fitness(individual_1)
+        assert res > 0
+
+    def test_comparation(self):
+        """
+        Comparation between indiviuals that gives a 5
+        """
+        result = get_two_carrot_input()
+        individual_1 = [['v', 'C', ' ', '<'],
+                        [' ', 'Z', ' ', ' '],
+                        [' ', ' ', ' ', ' '],
+                        [' ', ' ', 'Z', ' ']]
+        individual_2 = [[' ', 'C', ' ', 'v'],
+                        [' ', 'Z', ' ', ' '],
+                        [' ', ' ', ' ', ' '],
+                        [' ', ' ', 'Z', '<']]
+        child = result.new_child(individual_1, individual_2)
+        assert child != individual_1 or child != individual_2
